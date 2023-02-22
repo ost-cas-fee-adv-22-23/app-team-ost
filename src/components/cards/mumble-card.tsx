4ae28e3239e993ca43f6a0f041930ae78a92102b
Martin Thomann <martin.thomann@mgb.ch>
2023-02-22 15:11:01 +0100

    feat: make mumble-card more dynamic

--- src/components/cards/mumble-card.tsx
+++ src/components/cards/mumble-card.tsx
@@ -14,13 +14,14 @@ import {
   StackSpacing,
   UserShortRepresentation,
   UserShortRepresentationLabelType,
+  UserShortRepresentationProfilePictureSize,
 } from '@smartive-education/design-system-component-library-team-ost';
 import { FC } from 'react';
 import { MumbleType } from '../../types/public-api';
 
 export enum MumbleCardVariant {
   detailpage = 'detailpage',
-  respone = 'response',
+  response = 'response',
   timeline = 'timeline',
 }
 
@@ -35,6 +36,7 @@ type MumbleCardVariantMapType = {
   profilePictureSize: ProfilePictureSize;
   textSize: ParagraphSize;
   userShortRepresentationLabelType: UserShortRepresentationLabelType;
+  userShortRepresentationProfilePictureSize: UserShortRepresentationProfilePictureSize;
 };
 
 const contentCardvariantMap: Record<MumbleCardVariant, MumbleCardVariantMapType> = {
@@ -44,6 +46,7 @@ const contentCardvariantMap: Record<MumbleCardVariant, MumbleCardVariantMapType>
     profilePictureSize: ProfilePictureSize.m,
     textSize: ParagraphSize.l,
     userShortRepresentationLabelType: UserShortRepresentationLabelType.l,
+    userShortRepresentationProfilePictureSize: UserShortRepresentationProfilePictureSize.s,
   },
   response: {
     borderRadiusType: BorderRadiusType.none,
@@ -51,6 +54,7 @@ const contentCardvariantMap: Record<MumbleCardVariant, MumbleCardVariantMapType>
     profilePictureSize: ProfilePictureSize.s,
     textSize: ParagraphSize.m,
     userShortRepresentationLabelType: UserShortRepresentationLabelType.s,
+    userShortRepresentationProfilePictureSize: UserShortRepresentationProfilePictureSize.s,
   },
   timeline: {
     borderRadiusType: BorderRadiusType.roundedFull,
@@ -58,25 +62,42 @@ const contentCardvariantMap: Record<MumbleCardVariant, MumbleCardVariantMapType>
     profilePictureSize: ProfilePictureSize.m,
     textSize: ParagraphSize.m,
     userShortRepresentationLabelType: UserShortRepresentationLabelType.m,
+    userShortRepresentationProfilePictureSize: UserShortRepresentationProfilePictureSize.s,
   },
 };
 
+//TODO Define state for like counter, handle the like click and pass a callback for handling the like click
 export const MumbleCard: FC<MumbleCardProps> = ({ variant, mumble }) => {
   const setting = contentCardvariantMap[variant] || contentCardvariantMap.detailpage;
 
   return (
     <Card borderRadiusType={setting.borderRadiusType} isInteractive={setting.isInteractive}>
-      <div className="absolute -left-l">
-        <ProfilePicture alt={mumble.creator.userName} size={setting.profilePictureSize} src={mumble.creator.avatarUrl} />
-      </div>
+      {!MumbleCardVariant.response && (
+        <div className="absolute -left-l">
+          <ProfilePicture alt={mumble.creator.userName} size={setting.profilePictureSize} src={mumble.creator.avatarUrl} />
+        </div>
+      )}
       <Stack direction={StackDirection.col} spacing={StackSpacing.s}>
-        <UserShortRepresentation
-          displayName={`${mumble.creator.firstName} ${mumble.creator.lastName}`}
-          hrefProfile="#"
-          labelType={setting.userShortRepresentationLabelType}
-          timestamp="vor 42 Minuten"
-          username={mumble.creator.userName}
-        />
+        {MumbleCardVariant.response ? (
+          <UserShortRepresentation
+            displayName={`${mumble.creator.firstName} ${mumble.creator.lastName}`}
+            hrefProfile="#"
+            labelType={setting.userShortRepresentationLabelType}
+            timestamp="vor 42 Minuten"
+            username={mumble.creator.userName}
+          />
+        ) : (
+          <UserShortRepresentation
+            alt={mumble.creator.userName}
+            displayName={`${mumble.creator.firstName} ${mumble.creator.lastName}`}
+            hrefProfile="#"
+            labelType={setting.userShortRepresentationLabelType}
+            profilePictureSize={setting.userShortRepresentationProfilePictureSize}
+            src={mumble.creator.avatarUrl!}
+            timestamp="vor 42 Minuten"
+            username={mumble.creator.userName}
+          />
+        )}
         <div className="text-slate-900">
           <Paragraph size={setting.textSize}>{mumble.text}</Paragraph>
         </div>
