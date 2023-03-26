8e6d291d9d11d97ad3a05e4ad9bee06564fac519
Nando Schär <schaer.nando@gmail.com>
2023-03-26 13:06:39 +0200

    feat: add timeAgo helper

--- src/components/cards/mumble-card.tsx
+++ src/components/cards/mumble-card.tsx
@@ -19,6 +19,7 @@ import {
 import { FC } from 'react';
 import { Mumble } from '../../types/mumble';
 import Link from 'next/link';
+import { timeAgo } from '../../helpers/time-ago';
 
 // todo: bessere Namen für Variants. Evtl. eigene Components für Variants erstellen
 export enum MumbleCardVariant {
@@ -87,7 +88,7 @@ export const MumbleCard: FC<MumbleCardProps> = ({ variant, mumble }) => {
             hrefProfile={mumble.creator.profileUrl}
             labelType={settings.userShortRepresentationLabelType}
             linkComponent={Link}
-            timestamp={mumble.createdAt}
+            timestamp={timeAgo(mumble.createdAt)}
             username={mumble.creator.userName}
           />
         ) : (
@@ -99,7 +100,7 @@ export const MumbleCard: FC<MumbleCardProps> = ({ variant, mumble }) => {
             linkComponent={Link}
             profilePictureSize={settings.userShortRepresentationProfilePictureSize}
             src={mumble.creator.avatarUrl || ''}
-            timestamp={mumble.createdAt}
+            timestamp={timeAgo(mumble.createdAt)}
             username={mumble.creator.userName}
           />
         )}
