61df3feb76b6f358b3d76fba642ae81c65678b3d
martinthomann <mthomann@me.com>
2023-04-08 21:44:35 +0200

    feat: add page transition and key with route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
@@ -10,11 +11,13 @@ const poppins = Poppins({
   variable: '--font-poppins',
 });
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, router, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <main className={`${poppins.variable} font-poppins`}>
-        <Component {...pageProps} />
+        <AnimatePresence mode="wait" initial={false}>
+          <Component {...pageProps} key={router.asPath} />
+        </AnimatePresence>
       </main>
     </SessionProvider>
   );
