db7e208fac07c57cd7931e14dc5301846e1d0623
martinthomann <mthomann@me.com>
2023-05-01 22:12:00 +0200

    feat: setup inital playwright tests

---
+++ src/tests/e2e/example.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Check Title', () => {
+  test('has title timeline', async ({ page }) => {
+    await page.goto(process.env.NEXT_PUBLIC_URL as string);
+    //Expect a title to contain a substring.
+    await expect(page).toHaveTitle(/Timeline/);
+  });
+});
+
+test.describe('New Mumble', () => {
+  test('should add a new mumble', async ({ page }) => {
+    const testdate = Date.now().toString();
+
+    await page.goto(process.env.NEXT_PUBLIC_URL as string);
+    const loginButton = page.getByText('Login');
+    await loginButton.click();
+
+    const loginWithZitadel = page.getByText('Anmelden mit ZITADEL');
+    await loginWithZitadel.click();
+
+    const loginnameField = page.getByPlaceholder('username');
+    await loginnameField.fill(process.env.ZITADEL_USERNAME as string);
+    const forwardButton = page.getByText('next');
+    await forwardButton.click();
+
+    const passwordField = page.getByLabel('password');
+    await passwordField.fill(process.env.ZITADEL_PASSWORD as string);
+    const forwardPwButton = page.getByText('next');
+    await forwardPwButton.click();
+
+    // Create locators for textarea and send button
+    const newMumble = page.getByPlaceholder('Und was meinst du dazu?');
+    const sendButton = page.getByText('Absenden');
+
+    // Create new mumble and submit
+    await newMumble.fill(`Playwright Test - ${testdate}`);
+    await sendButton.click();
+
+    //Expect a new mumble
+    await expect(page.getByText(testdate)).toBeVisible();
+  });
+});
