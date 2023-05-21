ea320975c0131e55a8d99ca56cdf62a4d15e31a3
Martin Thomann <martin.thomann@mgb.ch>
2023-05-21 19:56:56 +0200

    feat: add tests

--- src/tests/e2e/mumble.spec.ts
+++ src/tests/e2e/mumble.spec.ts
@@ -8,10 +8,8 @@ test.describe('Check Title', () => {
   });
 });
 
-test.describe('New Mumble', () => {
-  test('should add a new mumble', async ({ page }) => {
-    const testdate = Date.now().toString();
-
+test.describe('Create a new Mumble', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(process.env.NEXT_PUBLIC_URL as string);
     await page.getByRole('link', { name: 'Login' }).click();
 
@@ -26,6 +24,10 @@ test.describe('New Mumble', () => {
     await passwordField.fill(process.env.ZITADEL_PASSWORD as string);
     const forwardPwButton = page.getByText('next');
     await forwardPwButton.click();
+  });
+
+  test('should add a new mumble', async ({ page }) => {
+    const testdate = Date.now().toString();
 
     // Create locators for textarea and send button
     const newMumble = page.getByPlaceholder('Und was meinst du dazu?');
@@ -38,5 +40,38 @@ test.describe('New Mumble', () => {
     //Expect a new mumble
     await page.goto(process.env.NEXT_PUBLIC_URL as string);
     await expect(page.getByText(testdate)).toBeVisible();
+  });
+
+  test('should upload an image', async ({ page }) => {
+    //Todo upload image
+  });
+});
+
+test.describe('Like a Mumble', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(process.env.NEXT_PUBLIC_URL as string);
+    await page.getByRole('link', { name: 'Login' }).click();
+
+    await page.getByRole('button', { name: 'login with zitadel' }).click();
+
+    const loginnameField = page.getByPlaceholder('username');
+    await loginnameField.fill(process.env.ZITADEL_USERNAME as string);
+    const forwardButton = page.getByText('next');
+    await forwardButton.click();
+
+    const passwordField = page.getByLabel('password');
+    await passwordField.fill(process.env.ZITADEL_PASSWORD as string);
+    const forwardPwButton = page.getByText('next');
+    await forwardPwButton.click();
+  });
+
+  test('Likes a new mumble and check counts', async ({ page }) => {
+    // Create locators for textarea and send button
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('button', { name: 'Like' }).click();
+
+    //Expect a new mumble
+    await page.goto(process.env.NEXT_PUBLIC_URL as string);
+    await expect(page.getByText('testdate')).toBeVisible();
   });
 });
