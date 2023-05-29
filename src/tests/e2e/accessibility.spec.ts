617dfcd64585091188ba84cc0315fc41c6d1b1c7
martinthomann <mthomann@me.com>
2023-05-29 22:04:12 +0200

    feat: add a11y testing

---
+++ src/tests/e2e/accessibility.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.describe('A11y test', () => {
+  test('a11y test with result attachement', async ({ page }, testInfo) => {
+    await page.goto(process.env.NEXT_PUBLIC_URL as string);
+
+    // We disable some rules here, as we have some known issues with them
+    // We should fix them in the future and enable the rules again
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(['color-contrast', 'html-has-lang', 'duplicate-id', 'heading-order'])
+      .analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json',
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
