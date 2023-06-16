01969abfc999125042056e08e371b30adc04894c
martinthomann <mthomann@me.com>
2023-06-16 21:56:43 +0200

    test: improve auth setup

--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -7,6 +7,8 @@ setup('authenticate as test user', async ({ page }) => {
 
   await page.getByRole('button', { name: 'login with zitadel' }).click();
 
+  await expect(page).toHaveURL(/.*.zitadel.cloud\/ui\/login\/login.*/);
+
   const loginnameField = await page.waitForSelector('input[name="loginName"]');
   loginnameField.fill(process.env.ZITADEL_USERNAME as string);
   const forwardButton = page.getByText('next');
